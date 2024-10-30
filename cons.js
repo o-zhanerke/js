edd51a2426cb62c0b563fd9aa373b5845bb95baf
Zhanerke Omiralikyzy <zhanerke.omiralikyzy@mail.ru>
2024-10-30 18:49:34 +0500

    hw started

---
+++ cons.js
@@ -0,0 +1,10 @@
+var name = "Zhanerke";
+var surname = "Omiralikyzy";
+var birthday = " 04.10.2007";
+var otan = " Kazakhstan";
+
+console.log("Menin atym", name);
+console.log("Menin tegim",surname);
+console.log("Menin tugan kunim", birthday);
+console.log("Menin otanym",otan);
+
