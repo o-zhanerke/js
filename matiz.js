f121c3e2a8d3f2271e6dc208749807f9adfbcb25
Zhanerke Omiralikyzy <zhanerke.omiralikyzy@mail.ru>
2024-11-04 18:34:08 +0500

    hw30

---
+++ matiz.js
@@ -0,0 +1,11 @@
+var modelName = "Mazda";
+var color = "Сары";
+var wheelsCount = "4";
+var reiting = "Максимально возможная оценка - 10 баллов.";
+var isOffRoad = "Жоқ";
+
+console.log("Модель атауы:", modelName);
+console.log("Түсі:", color);
+console.log("Дөңгелек саны:", wheelsCount);
+console.log("Рейтингі:", reiting);
+console.log("Жолсыз аймақпен (бездорожье) жүруге арналған ба?", isOffRoad);
